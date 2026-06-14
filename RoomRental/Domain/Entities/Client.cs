7f3e4d95c3d494717afc719576176b2b6589a50d
MTV <m.artemev@g.nsu.ru>
2026-06-14 22:01:07 +0700

    update methods added

--- RoomRental/Domain/Entities/Client.cs
+++ RoomRental/Domain/Entities/Client.cs
@@ -10,8 +10,40 @@ public class Client
     public Client(Guid id, string fullName, string email, string phoneNumber)
     {
         Id = id;
-        FullName = fullName;
-        Email = email;
-        PhoneNumber = phoneNumber;
+        UpdateFullName(fullName);
+        UpdateEmail(email);
+        UpdatePhone(phoneNumber);
+    }
+
+    public void UpdateFullName(string fullName)
+    {
+        if (string.IsNullOrWhiteSpace(fullName))
+            throw new ArgumentException("Name cannot be null or whitespace",nameof(fullName));
+        if (fullName.Length > 100)
+            throw new ArgumentException("Name cannot be longer than 100 characters", nameof(fullName));
+        FullName = fullName.Trim();
+    }
+
+    public void UpdateEmail(string email)
+    {
+        if (string.IsNullOrWhiteSpace(email))
+            throw new ArgumentException("Email cannot be null or whitespace", nameof(email));
+        if (email.Length > 200)
+            throw new ArgumentException("Email cannot be longer than 200 characters", nameof(email));
+        if (!email.Contains('@') || !email.Contains('.'))
+            throw new ArgumentException("Email is not a valid email address", nameof(email));
+        Email = email.Trim().ToLower();
+    }
+
+    public void UpdatePhone(string phone)
+    {
+        if (string.IsNullOrWhiteSpace(phone))
+            throw new ArgumentException("Phone cannot be empty", nameof(phone));
+    
+        var cleanPhone = new string(phone.Where(char.IsDigit).ToArray());
+        if (cleanPhone.Length < 10)
+            throw new ArgumentException("Phone must contain at least 10 digits", nameof(phone));
+    
+        PhoneNumber = phone.Trim();
     }
 }
