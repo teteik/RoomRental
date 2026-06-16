c32e2ca542798b6ad0ec7ad6d8ad52a4035aca58
MTV <artemjev.motya@yandex.ru>
2026-06-16 22:01:41 +0700

    complete CRUD for Room entity

--- RoomRental.API/DTOs/UpdateRoomRequest.cs
+++ RoomRental.API/DTOs/UpdateRoomRequest.cs
@@ -2,7 +2,7 @@ namespace RoomRental.API.DTOs;
 
 public class UpdateRoomRequest
 {
-    public required string Name { get; set; } 
+    public required string Name { get; set; }
     public int Capacity { get; set; }
     public decimal PricePerHour { get; set; }
     public string? Description { get; set; }
