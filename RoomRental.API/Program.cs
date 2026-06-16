074a989067b841953032ec9cd67e26bcc3b9e9a1
MTV <m.artemev@g.nsu.ru>
2026-06-16 20:16:37 +0700

    add API layer

---
+++ RoomRental.API/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using RoomRental.Infrastructure.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
