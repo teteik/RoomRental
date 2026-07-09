e9538374f4df6ca925ed6e65e8c20da1a72cd84f
teteik <artemjev.motya@yandex.ru>
2026-07-09 22:24:23 +0700

    add applicationUser

---
+++ RoomRental.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace RoomRental.Domain.Entities;
+
+public class ApplicationUser : IdentityUser
+{
+    
+}
--- RoomRental.Infrastructure/Data/AppDbContext.cs
+++ RoomRental.Infrastructure/Data/AppDbContext.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using RoomRental.Domain.Entities;
 
 namespace RoomRental.Infrastructure.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext<ApplicationUser>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
